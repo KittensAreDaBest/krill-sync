0de3e7549840a4f1e8001acf5c2095e10bec93de
Mythical <60989180+KittensAreDaBest@users.noreply.github.com>
2023-07-22 02:21:44 +1000

    add source_uri_base

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,9 @@ DATA="${DATA:-/data}"
 RRDP_DIR="${RRDP_DIR:-$DATA/rrdp}"
 RSYNC_DIR="${RSYNC_DIR:-$DATA/rsync}"
 STATE_DIR="${STATE_DIR:-$DATA/state}"
-
 exec /usr/local/bin/krill-sync \
     -v \
+    --source_uri_base ${SOURCE_URI_BASE} \ 
     --rrdp-dir ${RRDP_DIR} \
     --rsync-dir ${RSYNC_DIR} \
     --state-dir ${STATE_DIR} \
